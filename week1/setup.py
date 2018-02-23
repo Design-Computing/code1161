cfbfe5d6bb8a22adcd0313ae08fac7d3cd4e2e63
Ishaan Varshney <ishaan.varshney@gmail.com>
2018-02-23 17:33:00 +1100

    :wrench: do not install if already there

--- week1/setup.py
+++ week1/setup.py
@@ -1,10 +1,12 @@
 import os
 
-# install virtualenv
-os.system('pip install virtualenv')
-# create virtualenv
-os.system('virtualenv venv')
-# activate the virtualenv
-os.system('source venv/bin/activate')
+if not os.path.exists('./venv'):
+    # install virtualenv
+    os.system('pip install virtualenv')
+    # create virtualenv
+    os.system('virtualenv venv')
+    # activate the virtualenv
+    os.system('source venv/bin/activate')
+
 # install python ext
 os.system('code --install-extension ms-python.python')
